267b8b6217c4164b96421bcb9dc72f648c6ed99c
Ido Ben-Hur <idoybh2@gmail.com>
2023-03-15 14:48:25 +0200

    SystemUI: Add click actions to combined QS header
    This is needed here as well with the new QS header style
    
    Change-Id: I2020186af0da76f37c0a7cf5cc303d554d602fa7
    [ghostrider-reborn: adapt to 13-qpr3, improve code style]
    [someone5678: Remove Clock short click action as it's supported by default on AOSP in Android 14]
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -22,16 +22,20 @@ import android.annotation.ColorInt
 import android.annotation.IdRes
 import android.app.PendingIntent
 import android.app.StatusBarManager
-import android.content.Intent
 import android.content.Context
+import android.content.Intent
 import android.content.res.ColorStateList
 import android.content.res.Configuration
 import android.graphics.Color
 import android.graphics.Insets
+import android.net.Uri
 import android.os.Bundle
 import android.os.Trace
 import android.os.Trace.TRACE_TAG_APP
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.provider.AlarmClock
+import android.provider.CalendarContract
 import android.view.DisplayCutout
 import android.view.View
 import android.view.WindowInsets
@@ -107,7 +111,7 @@ constructor(
     private val nextAlarmController: NextAlarmController,
     private val activityStarter: ActivityStarter,
     private val statusOverlayHoverListenerFactory: StatusOverlayHoverListenerFactory,
-) : ViewController<View>(header), Dumpable {
+) : ViewController<View>(header), Dumpable, View.OnClickListener, View.OnLongClickListener {
 
     private val insetsProvider = insetsProviderStore.defaultDisplay
 
@@ -145,6 +149,7 @@ constructor(
     private val mShadeCarrierGroup: ShadeCarrierGroup = header.requireViewById(R.id.carrier_group)
     private val systemIconsHoverContainer: View =
         header.requireViewById(R.id.hover_system_icons_container)
+    private val vibrator: Vibrator = header.context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
     private var roundedCorners = 0
     private var cutout: DisplayCutout? = null
@@ -158,6 +163,7 @@ constructor(
             }
         }
 
+    private var privacyChipVisible = false
     private var qsDisabled = false
     private var visible = false
         set(value) {
@@ -209,6 +215,7 @@ constructor(
                 header.alpha = ShadeInterpolation.getContentAlpha(value)
                 field = value
                 updateIgnoredSlots()
+                updateVisibility()
             }
         }
 
@@ -262,6 +269,8 @@ constructor(
                 val update =
                     combinedShadeHeadersConstraintManager.privacyChipVisibilityConstraints(visible)
                 header.updateAllConstraints(update)
+                privacyChipVisible = visible
+                setBatteryClickable(qsExpandedFraction == 1f || !visible)
             }
         }
 
@@ -350,6 +359,43 @@ constructor(
         clock.setQsHeader()
 
         privacyIconsController.onParentVisible()
+
+        // click actions
+        clock.setOnLongClickListener(this)
+        date.setOnClickListener(this)
+        date.setOnLongClickListener(this)
+        setBatteryClickable(true)
+    }
+
+    override fun onClick(v: View) {
+        when (v) {
+            date -> {
+                val uri
+                    = CalendarContract.CONTENT_URI.buildUpon()
+                        .appendPath("time")
+                        .appendPath(System.currentTimeMillis().toString())
+                        .build()
+                activityStarter.postStartActivityDismissingKeyguard(
+                    Intent(Intent.ACTION_VIEW, uri), 0
+                )
+            }
+            batteryIcon -> activityStarter.postStartActivityDismissingKeyguard(
+                Intent(Intent.ACTION_POWER_USAGE_SUMMARY), 0
+            )
+        }
+    }
+
+    override fun onLongClick(v: View): Boolean {
+        if (v == clock || v == date) {
+            val intent = Intent(Intent.ACTION_MAIN).setClassName(
+                "com.android.settings",
+                "com.android.settings.Settings\$DateTimeSettingsActivity"
+            )
+            activityStarter.startActivity(intent, true /* dismissShade */)
+            vibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE))
+            return true
+        }
+        return false
     }
 
     override fun onViewAttached() {
@@ -547,6 +593,7 @@ constructor(
             header.progress = qsExpandedFraction
             updateBatteryMode()
         }
+        setBatteryClickable(qsExpandedFraction == 1f || !privacyChipVisible)
     }
 
     private fun logInstantEvent(message: String) {
@@ -612,6 +659,11 @@ constructor(
             clockPaddingEnd,
             clock.paddingBottom,
         )
+    }
+
+    private fun setBatteryClickable(clickable: Boolean) {
+        batteryIcon.setOnClickListener(if (clickable) this else null)
+        batteryIcon.setClickable(clickable)
     }
 
     override fun dump(pw: PrintWriter, args: Array<out String>) {

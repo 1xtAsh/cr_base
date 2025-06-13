Havoc-OS/android_frameworks_base

SystemUI: Add haptic feedback to QS tile press
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -37,6 +37,7 @@ import android.metrics.LogMaker;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.os.VibrationEffect;
 import android.service.quicksettings.Tile;
 import android.text.format.DateUtils;
 import android.util.ArraySet;
@@ -68,6 +69,8 @@ import com.android.systemui.qs.QSEvent;
 import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.QuickStatusBarHeader;
 import com.android.systemui.qs.logging.QSLogger;
+
+import com.havoc.support.util.VibrationUtils;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -254,6 +257,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                 getInstanceId());
         mQSLogger.logTileClick(mTileSpec, mStatusBarStateController.getState(), mState.state);
         mHandler.sendEmptyMessage(H.CLICK);
+        VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_CLICK);
     }
 
     public void secondaryClick() {
@@ -265,6 +269,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
         mQSLogger.logTileSecondaryClick(mTileSpec, mStatusBarStateController.getState(),
                 mState.state);
         mHandler.sendEmptyMessage(H.SECONDARY_CLICK);
+        VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_TICK);
     }
 
     public void longClick() {

1xtAsh/cr_base

packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -39,6 +39,7 @@ import android.metrics.LogMaker;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.os.VibrationEffect;
 import android.text.format.DateUtils;
 import android.util.ArraySet;
 import android.util.Log;
@@ -71,6 +72,8 @@ import com.android.systemui.qs.QsEventLogger;
 import com.android.systemui.qs.SideLabelTileLayout;
 import com.android.systemui.qs.flags.QsInCompose;
 import com.android.systemui.qs.logging.QSLogger;
+
+import org.derpfest.support.util.VibrationUtils;
 
 import java.io.PrintWriter;
 import java.util.Objects;
@@ -299,6 +302,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
         if (!mFalsingManager.isFalseTap(FalsingManager.LOW_PENALTY)) {
             mHandler.obtainMessage(H.CLICK, eventId, 0, expandable).sendToTarget();
         }
+        VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_CLICK);
     }
 
     @Override
@@ -312,6 +316,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
         mQSLogger.logTileSecondaryClick(mTileSpec, mStatusBarStateController.getState(),
                 mState.state, eventId);
         mHandler.obtainMessage(H.SECONDARY_CLICK, eventId, 0, expandable).sendToTarget();
+        VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_TICK);
     }
 
     @Override
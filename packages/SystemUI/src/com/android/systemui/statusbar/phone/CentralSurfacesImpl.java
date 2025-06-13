SuperiorOS/android_frameworks_base

SystemUI: Add 7-days Reboot Reminder/Suggestion
Personal preference :p

TODO:
- Make it configurable (maybe).

packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -230,6 +230,7 @@ import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.UserInfoControllerImpl;
 import com.android.systemui.statusbar.window.StatusBarWindowController;
 import com.android.systemui.statusbar.window.StatusBarWindowStateController;
+import com.android.systemui.superior.RebootSuggestion;
 import com.android.systemui.surfaceeffects.ripple.RippleShader.RippleShape;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.DumpUtilsKt;
@@ -446,6 +447,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
     private final StatusBarSignalPolicy mStatusBarSignalPolicy;
     private final StatusBarHideIconsForBouncerManager mStatusBarHideIconsForBouncerManager;
     private final Lazy<LightRevealScrimViewModel> mLightRevealScrimViewModelLazy;
+    private final RebootSuggestion mRebootSuggestion;
 
     /** Controller for the Shade. */
     private final ShadeSurface mShadeSurface;
@@ -864,6 +866,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         if (predictiveBackSysui()) {
             mContext.getApplicationInfo().setEnableOnBackInvokedCallback(true);
         }
+        mRebootSuggestion = new RebootSuggestion(mContext);
     }
 
     private void initBubbles(Bubbles bubbles) {

1xtAsh/cr_base

Change-Id: Ia619d851302e7ae57867b0ee0bb4ce40a76edd2f

packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -230,6 +230,7 @@ import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.UserInfoControllerImpl;
 import com.android.systemui.statusbar.window.StatusBarWindowControllerStore;
 import com.android.systemui.statusbar.window.StatusBarWindowStateController;
+import com.android.systemui.derpfest.RebootSuggestion;
 import com.android.systemui.surfaceeffects.ripple.RippleShader.RippleShape;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.DumpUtilsKt;
@@ -438,6 +439,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
     private final NotificationsController mNotificationsController;
     private final StatusBarSignalPolicy mStatusBarSignalPolicy;
     private final StatusBarHideIconsForBouncerManager mStatusBarHideIconsForBouncerManager;
+    private final RebootSuggestion mRebootSuggestion;
 
     /** Controller for the Shade. */
     private final ShadeSurface mShadeSurface;
@@ -879,6 +881,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         if (PredictiveBackSysUiFlag.isEnabled()) {
             mContext.getApplicationInfo().setEnableOnBackInvokedCallback(true);
         }
+        mRebootSuggestion = new RebootSuggestion(mContext);
     }
 
     private void initBubbles(Bubbles bubbles) {